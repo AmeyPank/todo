f3bdac8f84570a7a159cf2eab2533b83055c76a7
AmeyPank <pankantiameya@gmail.com>
2022-12-29 17:43:28 +0530

    commited to todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-List With Angular';
+  title = 'Todo List With Angular';
+list: any []=[];
   // constructor(){
   //   setTimeout(() => {
   //     this.title="Changed title";
   //   },2000);
-addTask(item:string){
-console.warn(item);
+  addTask(item:string){
+    
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list)
 }
+removeTask(id:number)
+{
+    console.warn(id)
+    this.list=this.list.filter((item: { id: number; })=>{
+      return item.id !== id;
+    });
+}
+
 }
 
